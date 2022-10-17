a2481b222a74787c0c66ded75cc4802458404d71
Catherine963 <catherine.e.deabreu@gmail.com>
2022-10-16 22:02:38 -0300

    [ADD] archivos

--- js/product-info.js
+++ js/product-info.js
@@ -7,30 +7,53 @@ document.addEventListener("DOMContentLoaded", () => {
     const containerComents = document.getElementById('coment-container');
 
 
+    let urlParams = new URLSearchParams(window.location.search);
+    let nextProductId = urlParams.get("nextProductId")
+
+    productoClickeado = (nextProductId != null) ? nextProductId : productoClickeado
+
+
     let htmlContentToAppend = "";
     fetch(`https://japceibal.github.io/emercado-api/products/${productoClickeado}.json`)
         .then(response => response.json())
         .then(data => {
 
             let imagenesHtml = '';
-            for (let img of data.images){
+            for (let img of data.images) {
                 imagenesHtml += `<div class="col"><img class="img-fluid" src="${img}"/></div>`
             }
-            let imagenesRow = `<div class="row"><p><br><b>Imágenes ilustrativas</b></br></p>${imagenesHtml}</div>`
+            let imagenesRow = `<div class="row"><h4 class="my-5"><br><b>Imágenes ilustrativas</b></br></h4>${imagenesHtml}</div>`
             let relacionados = '';
-            for (let rel of data.relatedProducts){
-                relacionados += `<div class="col-3">${rel.name} <img class="img-fluid" src="${rel.image}"/></div>`
+            for (let rel of data.relatedProducts) {
+                relacionados += `
+                <div class="col col-md-2 border">
+                    <a style="text-decoration:none; color:black;" href="/product-info.html?nextProductId=${rel.id}"> 
+                    <div id="${rel.id}">
+                        <img class="img-fluid pb-3" src="${rel.image}"/>
+                        <div>${rel.name} </div>
+                    </div>
+                    </a>
+                </div>`
             }
-            let relacionadosRow = `<div class="row"><p><br><b>Productos relacionados</b></br></p>${relacionados}  </div>`
+            let relacionadosRow = `
+                <div class="row">
+                <h4 class="my-5"><br><b>Productos relacionados</b></br></h4>
+                ${relacionados}
+                </div>
+            `
 
-        htmlContentToAppend += `
+
+
+
+
+            htmlContentToAppend += `
 
         <div id="(${data.id})" >
             <div class="row">
             
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">${data.name}</h4>
+                        <h2 class="mt-4">${data.name}</h4>
                         
                     </div><hr/>
                     <p  class="mb-1"><b>Precio</b> <br>${data.cost}</br></p>
@@ -42,57 +65,57 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
             ${imagenesRow}
             ${relacionadosRow}
+            
         </div>       
         `
-        containerProduct.innerHTML = htmlContentToAppend; //Agregar los elementos del contenedor
-        
-    });
+            containerProduct.innerHTML = htmlContentToAppend; //Agregar los elementos del contenedor
+
+        });
 
 
-    
-    function obtenerEstrellas(score){
+
+    function obtenerEstrellas(score) {
         let estrellas = '';
-        for(let i=1; i <= score; i++){
-               estrellas += '<span class="fa fa-star checked"></span>'
+        for (let i = 1; i <= score; i++) {
+            estrellas += '<span class="fa fa-star checked"></span>'
         }
         return estrellas
-            
+
     }
 
     let htmlContentToAppend1 = "";
     fetch(`https://japceibal.github.io/emercado-api/products_comments/${productoClickeado}.json`)
         .then(response => response.json())
         .then(data => {
-            
-        
+
+
             let comentsHtml = "";
-        for (let com of data){
-            let estrellasObtenidas = obtenerEstrellas(com.score);
-            comentsHtml += `
+            for (let com of data) {
+                let estrellasObtenidas = obtenerEstrellas(com.score);
+                comentsHtml += `
             <div class="col"><hr/>
                     <p  class="mb-1"><b>${com.user}</b> - ${com.dateTime}  ${estrellasObtenidas}</p> 
                     <p  class="mb-1">${com.description}</p>
             </div>                  
         `
-        }
+            }
 
-        htmlContentToAppend1 += `
+            htmlContentToAppend1 += `
         <div class="row">
-        <div class="col"><p>Comentarios</p>${comentsHtml}
+        <div class="col"><h4 class="my-5">Comentarios</h4>${comentsHtml}
         </div>
         </div>
         <div class="row">
         <div class="col">
-        <form class="form-control" action=""><p>Comentar</p><p>tu opinion</p><textarea class="w-100 p-3 form-control" name="" id=""  rows="10" ></textarea><p>Puntuacion</p><input class="form-control" type="number" max=5 min=0 > <input type="submit" class="btn btn-primary"> </form>
+        <form class="form-control mt-5" action=""><p>Comentar</p><textarea placeholder="Escribe tu comentario aqui..." class="w-100 p-3 form-control" name="" id=""  rows="10" ></textarea><label class="form-label mt-3"> Puntuacion</label><input class="form-control" type="number" max=5 min=0 > <input type="submit" class="btn btn-primary my-3"> </form>
         
         </div>
         </div>
         `
-        containerComents.innerHTML = htmlContentToAppend1; //Agregar los elementos del contenedor
+            containerComents.innerHTML = htmlContentToAppend1; //Agregar los elementos del contenedor
 
-    });
-    
-    
+        });
+
 
 });
 
